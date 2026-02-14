ea30937d59cf9bb9e495107965beec2f7dc8c72c
reg31 <37986256+reg31@users.noreply.github.com>
2026-02-14 16:19:51 +0000

    Update qvariant.h

--- files/qvariant.h
+++ files/qvariant.h
@@ -358,16 +358,8 @@ public:
     template<typename T> inline T value() const & { return qvariant_cast<T>(*this); }
     template<typename T> inline T value() && { return qvariant_cast<T>(std::move(*this)); }
 
-    template<typename T>
-    inline T view()
-    {
-        T t{};
-        QMetaType::view(metaType(), data(), QMetaType::fromType<T>(), &t);
-        return t;
-    }
-
-    template<typename T>
-    bool canView() const { return canView(QMetaType::fromType<T>()); }
+    template<typename T> inline T view() { T t{}; QMetaType::view(metaType(), data(), QMetaType::fromType<T>(), &t); return t; }
+    template<typename T> bool canView() const { return canView(QMetaType::fromType<T>()); }
 
     template<typename T>
     static inline QVariant fromValue(T &&value) noexcept(std::is_nothrow_copy_constructible_v<std::remove_cvref_t<T>> && Private::CanUseInternalSpace<std::remove_cvref_t<T>>) requires (std::is_copy_constructible_v<std::remove_cvref_t<T>> && std::is_destructible_v<std::remove_cvref_t<T>>)
