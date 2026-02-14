9c6f258718bd004bbf79f12d94dff8b2a392357e
reg31 <37986256+reg31@users.noreply.github.com>
2026-02-14 11:38:06 +0000

    Update qsgmaterialshader.cpp

--- files/qsgmaterialshader.cpp
+++ files/qsgmaterialshader.cpp
@@ -1,17 +1,6 @@
 // Copyright (C) 2019 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR GPL-2.0-only OR GPL-3.0-only
 
-#include "qsgmaterial.h"
-#include "qsgrenderer_p.h"
-#include "qsgmaterialshader_p.h"
-#include <QtCore/QFile>
-
-
-#include <flat_map>
-#include <mutex>
-#include <shared_mutex>
-
-
 QT_BEGIN_NAMESPACE
 
 /*!
