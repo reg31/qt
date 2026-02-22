c7c284c7ea19fab39dabfce52aa56e8425b1b8c7
reg31 <37986256+reg31@users.noreply.github.com>
2026-02-22 17:19:09 +0000

    Update lld-wrapper.cmd

--- files/lld-wrapper.cmd
+++ files/lld-wrapper.cmd
@@ -1,13 +1,15 @@
 @echo off
-powershell -Command "
-    $args = '%*' -split ' '
-    $pipe = New-Object System.IO.Pipes.NamedPipeClientStream('.', 'lld-wrapper-pipe', [System.IO.Pipes.PipeDirection]::InOut)
-    $pipe.Connect(5000)
-    $writer = New-Object System.IO.StreamWriter($pipe)
-    $writer.AutoFlush = $true
-    $reader = New-Object System.IO.StreamReader($pipe)
-    $writer.WriteLine(($args | ConvertTo-Json -Compress))
-    $exit = $reader.ReadLine()
-    $pipe.Dispose()
-    exit [int]$exit
-"
+setlocal enabledelayedexpansion
+set GXX=C:\ProgramData\mingw64\mingw64\bin\x86_64-w64-mingw32-g++.exe
+set ARGS=
+:loop
+if "%~1"=="" goto done
+set "ARG=%~1"
+if "!ARG:~0,21!"=="-Wl,--version-script," goto skip
+set "ARGS=!ARGS! "%~1""
+:skip
+shift
+goto loop
+:done
+%GXX% !ARGS!
+exit /b %ERRORLEVEL%
