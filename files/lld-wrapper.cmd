65a232fad0c34e91987715aec80257b77a230510
reg31 <37986256+reg31@users.noreply.github.com>
2026-02-22 15:56:06 +0000

    Update lld-wrapper.cmd

--- files/lld-wrapper.cmd
+++ files/lld-wrapper.cmd
@@ -1,21 +1,18 @@
 @echo off
 setlocal enabledelayedexpansion
+set COMPILER=%1
+shift
 set ARGS=
 :loop
 if "%~1"=="" goto done
-if "%~1"=="--version-script" (
-    shift
-    shift
-    goto loop
-)
 set "ARG=%~1"
-if "!ARG:~0,17!"=="--version-script=" (
+if "!ARG:~0,21!"=="-Wl,--version-script," (
     shift
     goto loop
 )
-set "ARGS=!ARGS! %1"
+set "ARGS=!ARGS! "%~1""
 shift
 goto loop
 :done
-"%LLVM_PATH%\bin\ld.lld" !ARGS!
+%COMPILER% !ARGS!
 exit /b %ERRORLEVEL%
